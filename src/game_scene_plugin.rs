9f13bea734f2a7bebac6ee2ca2278a99eae37803
gsharad007 <gsharad007@hotmail.com>
2024-02-27 18:25:41 -0500

    Adding map creation from string

--- src/game_scene_plugin.rs
+++ src/game_scene_plugin.rs
@@ -11,6 +11,8 @@ use bevy::{
     transform::components::Transform,
 };
 
+use itertools::Itertools;
+
 #[derive(Debug)]
 pub struct SceneElementsPlugin;
 
@@ -132,4 +134,111 @@ fn spawn_ceiling(
         transform: Transform::from_rotation(Quat::from_axis_angle(Vec3::X, FRAC_PI_2)),
         ..Default::default()
     });
+}
+
+#[test]
+fn test_map_load_string() {
+    let map_string = r"
+╞═╦╗
+╞═╬╣
+██║║
+╞═╩╝
+";
+
+    let map = Map::from_string(map_string);
+
+    // println!("{}", map_string);
+    // println!("{:?}", map.cells);
+
+    assert_eq!(map.cells[0][0], cell::OPEN_LEFT);
+    assert_eq!(map.cells[0][1], cell::OPEN_LEFT | cell::OPEN_RIGHT);
+    assert_eq!(
+        map.cells[0][2],
+        cell::OPEN_LEFT | cell::OPEN_RIGHT | cell::OPEN_BOTTOM
+    );
+    assert_eq!(map.cells[0][3], cell::OPEN_LEFT | cell::OPEN_BOTTOM);
+
+    assert_eq!(map.cells[1][0], cell::OPEN_LEFT);
+    assert_eq!(map.cells[1][1], cell::OPEN_LEFT | cell::OPEN_RIGHT);
+    assert_eq!(
+        map.cells[1][2],
+        cell::OPEN_LEFT | cell::OPEN_RIGHT | cell::OPEN_BOTTOM | cell::OPEN_TOP
+    );
+    assert_eq!(
+        map.cells[1][3],
+        cell::OPEN_LEFT | cell::OPEN_BOTTOM | cell::OPEN_TOP
+    );
+
+    assert_eq!(map.cells[2][0], cell::EMPTY);
+    assert_eq!(map.cells[2][1], cell::EMPTY);
+    assert_eq!(map.cells[2][2], cell::OPEN_TOP | cell::OPEN_BOTTOM);
+    assert_eq!(map.cells[2][3], cell::OPEN_TOP | cell::OPEN_BOTTOM);
+
+    assert_eq!(map.cells[3][0], cell::OPEN_LEFT);
+    assert_eq!(map.cells[3][1], cell::OPEN_LEFT | cell::OPEN_RIGHT);
+    assert_eq!(
+        map.cells[3][2],
+        cell::OPEN_LEFT | cell::OPEN_RIGHT | cell::OPEN_TOP
+    );
+    assert_eq!(map.cells[3][3], cell::OPEN_LEFT | cell::OPEN_TOP);
+}
+
+pub mod cell {
+    use derive_more::BitOr;
+
+    // #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, BitOr)]
+    // pub struct Type(u8);
+    pub type Type = u8;
+
+    pub const EMPTY: Type = 0b0;
+    pub const OPEN_TOP: Type = 0b1;
+    pub const OPEN_BOTTOM: Type = 0b10;
+    pub const OPEN_LEFT: Type = 0b100;
+    pub const OPEN_RIGHT: Type = 0b1000;
+}
+
+#[derive(Component, Debug)]
+pub struct Map {
+    pub cells: Vec<Vec<cell::Type>>,
+}
+
+impl Map {
+    pub fn from_string(map_string: &str) -> Self {
+        let cells = map_string
+            .lines()
+            .filter(|line| !line.is_empty())
+            .map(|line| {
+                line.chars()
+                    .map(|c| -> cell::Type {
+                        match c {
+                            // '█' => cell::EMPTY,
+                            '╨' => cell::OPEN_TOP,
+                            '╥' => cell::OPEN_BOTTOM,
+                            '╞' => cell::OPEN_LEFT,
+                            '╡' => cell::OPEN_RIGHT,
+                            '║' => cell::OPEN_TOP | cell::OPEN_BOTTOM,
+                            '═' => cell::OPEN_LEFT | cell::OPEN_RIGHT,
+                            '╝' => cell::OPEN_TOP | cell::OPEN_LEFT,
+                            '╚' => cell::OPEN_TOP | cell::OPEN_RIGHT,
+                            '╗' => cell::OPEN_BOTTOM | cell::OPEN_LEFT,
+                            '╔' => cell::OPEN_BOTTOM | cell::OPEN_RIGHT,
+                            '╠' => cell::OPEN_TOP | cell::OPEN_BOTTOM | cell::OPEN_RIGHT,
+                            '╣' => cell::OPEN_TOP | cell::OPEN_BOTTOM | cell::OPEN_LEFT,
+                            '╩' => cell::OPEN_TOP | cell::OPEN_LEFT | cell::OPEN_RIGHT,
+                            '╦' => cell::OPEN_BOTTOM | cell::OPEN_LEFT | cell::OPEN_RIGHT,
+                            '╬' => {
+                                cell::OPEN_TOP
+                                    | cell::OPEN_LEFT
+                                    | cell::OPEN_RIGHT
+                                    | cell::OPEN_BOTTOM
+                            }
+                            _ => cell::EMPTY,
+                        }
+                    })
+                    .collect_vec()
+            })
+            .collect_vec();
+
+        Self { cells }
+    }
 }

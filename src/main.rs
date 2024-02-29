cb45f40030d8d010983e38a2aa672dfba1668993
gsharad007 <gsharad007@hotmail.com>
2024-02-29 13:08:50 -0500

    Bigger maze

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod camera_controller_plugin;
 mod camera_setup_plugin;
 
-mod game_scene_plugin;
 mod game_cells_plugin;
+mod game_scene_plugin;
 
 use bevy::{app::App, DefaultPlugins};
 
@@ -13,8 +13,7 @@ use game_scene_plugin::SceneElementsPlugin;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(CameraSetupPlugin)
-        .add_plugins(CameraControllerPlugin)
+        .add_plugins((CameraSetupPlugin, CameraControllerPlugin))
         .add_plugins(SceneElementsPlugin)
         // .insert_resource(WindowDescriptor {
         //     title: "CodeRaid".to_string(),
